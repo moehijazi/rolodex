febee4d20a05652464c6bbb565f37e7bcac4734b
Mohamad Hijazi <hijazimohamad@hotmail.com>
2019-07-22 16:10:15 +0300

    Updates

---
+++ precache-manifest.d459915fa7f4aed162f8bdf00f377d9a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be26fc44fd2011fe2c6042fcef163ba6",
+    "url": "/rolodex/index.html"
+  },
+  {
+    "revision": "7123121750048c954497",
+    "url": "/rolodex/static/css/main.aad0b227.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "7123121750048c954497",
+    "url": "/rolodex/static/js/main.047352e6.chunk.js"
+  },
+  {
+    "revision": "41f11689102a27bddf9e",
+    "url": "/rolodex/static/js/runtime~main.230a6f71.js"
+  }
+]);
